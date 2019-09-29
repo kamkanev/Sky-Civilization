8f3e7a1809448fa5d5532db836f0b289a73c0afe
Kamen <kamkanev@gmail.com>
2019-09-29 13:50:28 +0300

    backTomain.js success

--- createGame.js
+++ createGame.js
@@ -2,12 +2,38 @@ const { remote } = require('electron');
 const { BrowserWindow } = require('electron').remote;
 const fs = require('fs');
 
-
 document.getElementById('startGameBtn').addEventListener('click', createGame);
+document.getElementById('startGameBackBtn').addEventListener('click', backToMain);
 
 function exit() {
   var window = remote.getCurrentWindow();
        window.close();
+}
+
+function backToMain() {
+  let win = new BrowserWindow({
+    width: 800,
+     height: 600,
+     resizable: false,
+     webPreferences: {
+             nodeIntegration: true
+         }
+    });
+  win.loadFile('index.html');
+
+  win.setMenuBarVisibility(false)
+
+  // Open the DevTools.
+   win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+    exit();
 }
 
 function createFile() {
