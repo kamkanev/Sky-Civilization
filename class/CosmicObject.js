0124c1f8e805cea2baf258e8c44bbce9ea7cfdc1
Kamen <kamkanev@gmail.com>
2019-10-13 11:31:35 +0300

    ima draw i size

--- class/CosmicObject.js
+++ class/CosmicObject.js
@@ -1,8 +1,15 @@
 class CosmicObject {
-  constructor(x, y, src) {
+  constructor(x, y, src, size) {
     this.x = x;
     this.y = y;
+    this.size = size;
     this.src = new Image();
     this.src.src = src;
+  }
+
+  draw(){
+
+    context.drawImage(this.src, this.x, this.y, this.size, this.size);
+
   }
 }
