79dba6c0276a469551eb208de82236fd60f3db75
Kamen <kamkanev@gmail.com>
2019-10-13 12:47:29 +0300

    load game files and window made

--- renderer.js
+++ renderer.js
@@ -7,6 +7,7 @@ let iconA = remote.nativeImage.createFromPath(pathI.join(__dirname, "images/icon
 
 document.getElementById('exit').addEventListener('click', exit);
 document.getElementById('new-game').addEventListener('click', createNewGame);
+document.getElementById('load-game').addEventListener('click', loadGame);
 
 function exit() {
   var window = remote.getCurrentWindow();
@@ -25,6 +26,35 @@ function createNewGame() {
          }
     });
   win.loadFile('newGame.html');
+
+  win.setMenuBarVisibility(false)
+
+  // Open the DevTools.
+   win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+
+  exit();
+}
+
+function loadGame() {
+
+  let win = new BrowserWindow({
+    width: 600,
+     height: 400,
+     resizable: false,
+     icon: iconA,
+     webPreferences: {
+             nodeIntegration: true
+         }
+    });
+  win.loadFile('loadGame.html');
 
   win.setMenuBarVisibility(false)
 
