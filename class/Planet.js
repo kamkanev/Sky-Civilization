6ab40e705fe5826b9d7ba83b1b2163ac8d8d4313
Kamen <kamkanev@gmail.com>
2019-10-19 18:01:43 +0300

    planets saves in json and infoButton click can

--- class/Planet.js
+++ class/Planet.js
@@ -6,12 +6,13 @@ class Planet extends CosmicObject {
       src = "Images/planets/g_ice.png"
     }else if(type == 2){
       src = "Images/planets/c_planet.png"
-    }else if(type == 4){
+    }else if(type == 3){
       src = "Images/planets/o_planet.png"
     }
     super(x, y, src, size, name);
     super.configMenu = false;
     this.type = type;
+    this.orbit = 3;
   }
 
   async setName(name){
